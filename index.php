1cea45998e898e3dd35306ce138da36273b0c6f7
Dima <dmitry@ablov.ru>
2021-04-16 17:31:09 +0300

    Закончен "сложный" варинант

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <title>Grammar test example</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="CSS/main.css" rel="stylesheet" type="text/css">
+    <!-- jQuery  -->
+    <script src="https://code.jquery.com/jquery-3.3.0.min.js"
+            integrity="sha256-RTQy8VOmNlT6b2PIRur37p6JEBZUE7o8wPgMvu18MC4="
+            crossorigin="anonymous"></script>
+    <script src="JS/main.js"></script>
+</head>
+<body>
+<!--<p>Cursor position: <span id="indicator"></span></p>-->
+<div class="wrap">
+    <textarea id="myText"></textarea>
+    <button id="check">Проверить</button>
+    <div id="result"></div>
+</div>
+</body>
